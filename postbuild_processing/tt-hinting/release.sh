7de1d2b1114bd7c8e559c6ee9272ecbae3240c0c
Chris Simpkins <git.simpkins@gmail.com>
2015-09-27 20:45:21 -0400

    repository reorganization, new hinting script, new release script, new ttf/otf release builds

---
+++ postbuild_processing/tt-hinting/release.sh
@@ -0,0 +1,8 @@
+cp ../posthinted_builds/Hack-Regular.ttf ../../build/ttf/Hack-Regular.ttf
+echo "moved hinted version of Hack-Regular.ttf to ../../build/ttf/Hack-Regular.ttf"
+cp ../posthinted_builds/Hack-Bold.ttf ../../build/ttf/Hack-Bold.ttf
+echo "moved hinted version of Hack-Bold.ttf to ../../build/ttf/Hack-Bold.ttf"
+cp ../posthinted_builds/Hack-Italic.ttf ../../build/ttf/Hack-Italic.ttf
+echo "moved hinted version of Hack-Italic.ttf to ../../build/ttf/Hack-Italic.ttf"
+cp ../posthinted_builds/Hack-BoldItalic.ttf ../../build/ttf/Hack-BoldItalic.ttf
+echo "moved hinted version of Hack-BoldItalic.ttf to ../../build/ttf/Hack-BoldItalic.ttf"
