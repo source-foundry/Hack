de6c4f9e796401f46e1020ef0483f4a05f163526
Chris Simpkins <git.simpkins@gmail.com>
2016-01-20 10:42:58 -0500

    updated tar.xz archive generator script

--- postbuild_processing/archive_prep/make_tarxz.sh
+++ postbuild_processing/archive_prep/make_tarxz.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-HACK_VERSION="v2.019"
+HACK_VERSION="v2_019"
 HACK_BUILD_DIR="../../build"
 
 
@@ -8,10 +8,10 @@ HACK_BUILD_DIR="../../build"
 cd "$HACK_BUILD_DIR"
 
 # Build ttf file archive
-tar c ttf --exclude=*.DS_Store | xz --extreme -9 --force > "Hack_${HACK_VERSION}-ttf.tar.xz"
+tar c --exclude=.DS_Store --exclude=./.DS_Store --exclude=./*/.DS_Store -C ttf . | xz --extreme -9 --force > "archives/Hack_${HACK_VERSION}-ttf.tar.xz"
 
 # Build otf file archive
-tar c otf --exclude=*.DS_Store | xz --extreme -9 --force  > "Hack_${HACK_VERSION}-otf.tar.xz"
+tar c --exclude=.DS_Store --exclude=./.DS_Store --exclude=./*/.DS_Store -C otf . | xz --extreme -9 --force  > "archives/Hack_${HACK_VERSION}-otf.tar.xz"
 
 # Build web font file archive
-tar c webfonts --exclude=*.DS_Store | xz --extreme -9 --force > "Hack_${HACK_VERSION}-webfonts.tar.xz"
+tar c --exclude=.DS_Store --exclude=./.DS_Store --exclude=./*/.DS_Store -C webfonts . | xz --extreme -9 --force > "archives/Hack_${HACK_VERSION}-webfonts.tar.xz"
