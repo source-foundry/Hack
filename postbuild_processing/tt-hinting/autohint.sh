6f7c6861a449d2061ee6b8efe0a0538a08e07eeb
Chris Simpkins <git.simpkins@gmail.com>
2016-03-16 20:27:51 -0400

    updated TrueType autohint shell script with new prehinted build paths

--- postbuild_processing/tt-hinting/autohint.sh
+++ postbuild_processing/tt-hinting/autohint.sh
@@ -1,7 +1,7 @@
 # ------------------------------------------------------------------
 #
 #  autohint.sh
-#  Copyright 2015 Christopher Simpkins
+#  Copyright 2016 Christopher Simpkins
 #  MIT license
 #
 # ------------------------------------------------------------------
@@ -12,17 +12,17 @@
 
 
 # Hack-Regular.ttf
-ttfautohint -l 4 -r 80 -G 350 -x 0 -H 181 -D latn -f latn -w G -W -t -X "" -I "../prehinted_builds/Hack-Regular.ttf" "../posthinted_builds/Hack-Regular.ttf"
-echo "Hack-Regular.ttf hinted and moved to ../posthinted_builds/Hack-Regular.ttf"
+ttfautohint -l 4 -r 80 -G 350 -x 0 -H 181 -D latn -f latn -w G -W -t -X "" -I "../prehinted_builds/Hack-Regular-TT.ttf" "../posthinted_builds/Hack-Regular.ttf"
+echo "Hack-Regular-TT.ttf hinted and moved to ../posthinted_builds/Hack-Regular.ttf"
 
 # Hack-Bold.ttf
-ttfautohint -l 4 -r 80 -G 350 -x 0 -H 260 -D latn -f latn -w G -W -t -X "" -I -m "Hack-Bold-TA.txt" "../prehinted_builds/Hack-Bold.ttf" "../posthinted_builds/Hack-Bold.ttf"
-echo "Hack-Bold.ttf hinted and moved to ../posthinted_builds/Hack-Bold.ttf"
+ttfautohint -l 4 -r 80 -G 350 -x 0 -H 260 -D latn -f latn -w G -W -t -X "" -I -m "Hack-Bold-TA.txt" "../prehinted_builds/Hack-Bold-TT.ttf" "../posthinted_builds/Hack-Bold.ttf"
+echo "Hack-Bold-TT.ttf hinted and moved to ../posthinted_builds/Hack-Bold.ttf"
 
 # Hack-Italic.ttf
-ttfautohint -l 4 -r 80 -G 350 -x 0 -H 145 -D latn -f latn -w G -W -t -X "" -I "../prehinted_builds/Hack-Italic.ttf" "../posthinted_builds/Hack-Italic.ttf"
-echo "Hack-Italic.ttf hinted and moved to ../posthinted_builds/Hack-Italic.ttf"
+ttfautohint -l 4 -r 80 -G 350 -x 0 -H 145 -D latn -f latn -w G -W -t -X "" -I "../prehinted_builds/Hack-Italic-TT.ttf" "../posthinted_builds/Hack-Italic.ttf"
+echo "Hack-Italic-TT.ttf hinted and moved to ../posthinted_builds/Hack-Italic.ttf"
 
 # Hack-BoldItalic.ttf
-ttfautohint -l 4 -r 80 -G 350 -x 0 -H 265 -D latn -f latn -w G -W -t -X "" -I "../prehinted_builds/Hack-BoldItalic.ttf" "../posthinted_builds/Hack-BoldItalic.ttf"
-echo "Hack-BoldItalic.ttf hinted and moved to ../posthinted_builds/Hack-BoldItalic.ttf"
+ttfautohint -l 4 -r 80 -G 350 -x 0 -H 265 -D latn -f latn -w G -W -t -X "" -I "../prehinted_builds/Hack-BoldItalic-TT.ttf" "../posthinted_builds/Hack-BoldItalic.ttf"
+echo "Hack-BoldItalic-TT.ttf hinted and moved to ../posthinted_builds/Hack-BoldItalic.ttf"
