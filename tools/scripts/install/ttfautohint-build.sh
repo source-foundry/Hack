1473810f108a48bc5a7cc071a583b1278ec02a2b
Chris Simpkins <git.simpkins@gmail.com>
2017-11-24 14:21:26 -0500

    converted ttfautohint-build.sh script to MIT License (see discussion https://github.com/source-foundry/ttfautohint-build/issues/8)

--- tools/scripts/install/ttfautohint-build.sh
+++ tools/scripts/install/ttfautohint-build.sh
@@ -5,11 +5,29 @@
 # This script builds a stand-alone binary for the command line version of
 # ttfautohint, downloading any necessary libraries.
 #
-# Version 2017-Aug-27.
-# Written by Werner Lemberg <wl@gnu.org>.
-#
-# To the extent possible under law, the person who associated CC0 with this work
-# has waived all copyright and related or neighboring rights to this work.
+# Version 2017-Nov-24.
+
+# The MIT License (MIT)
+
+# Copyright (c) 2017 Werner Lemberg
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 
 #
 # User configuration.
