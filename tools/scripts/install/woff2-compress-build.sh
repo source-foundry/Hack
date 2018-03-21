2a46cad575da4d7c4c48acb9232d111636150e1e
Chris Simpkins <git.simpkins@gmail.com>
2018-03-21 17:35:41 -0400

    [woff2-compress-build.sh] modified woff2_compress to use brotli v1.0.3

--- tools/scripts/install/woff2-compress-build.sh
+++ tools/scripts/install/woff2-compress-build.sh
@@ -14,7 +14,7 @@
 # The woff2 git clone directory.
 BUILD="$HOME"
 INST="$HOME/woff2"
-WOFF2_VERSION_TAG="v1.0.2"
+WOFF2_COMMIT="5e5f2cf20d5f9d3373a2d8b4905105f80646c014"
 
 # woff2 executable path
 WOFF2_BIN="$BUILD/woff2/woff2_compress"
@@ -37,8 +37,8 @@ cd "$INST" || exit 1
 
 # checkout desired version tag
 echo " "
-echo "Checking out woff2 version tag $WOFF2_VERSION_TAG"
-git checkout $WOFF2_VERSION_TAG
+echo "Checking out woff2 at commit $WOFF2_COMMIT"
+git checkout $WOFF2_COMMIT
 
 echo "#####"
 echo "Build woff2"
